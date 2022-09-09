58bf4e77ede4f7d43e48c9b2af37351bc9e703b0
PhysicsX <ulasdikme@gmail.com>
2022-09-09 21:07:51 +0200

    include process queue handler

--- include/processQueue.h
+++ include/processQueue.h
@@ -1,4 +1,78 @@
 #ifndef PROCESSQUEUE_H
 #define PROCESSQUEUE_H
 
+#include <iostream>
+#include <queue>
+#include <thread>
+#include <future>
+
+template <class T>
+class queueProcess {
+public:
+    queueProcess():
+        runFlag(false){}
+
+    ~queueProcess() {
+        {
+        std::lock_guard<std::mutex> lock{mMutex};
+        runFlag = true;
+        mCondVar.notify_one();
+        }
+        if(fut.valid())
+        {
+            fut.wait();
+        }
+    }
+
+  void addQueue(const T& process) {
+    {
+        std::lock_guard<std::mutex> lock{mMutex};
+        mQueue.push(process);
+        mCondVar.notify_one();
+    }
+
+  }
+
+  bool checkQueueIsEmpty()
+  {
+      std::lock_guard<std::mutex> lock{mMutex};
+      return mQueue.size() ? true : false;
+  }
+
+  void processQueue() {
+
+    if(!fut.valid())
+    {
+    fut = std::async(std::launch::async, [&](){
+
+      while (!runFlag) {
+           std::unique_lock<std::mutex> lock{mMutex};
+
+           mCondVar.wait(
+                lock, [this]() { return !mQueue.empty() || runFlag; });
+
+          if(runFlag)
+          {
+                return;
+          }
+
+           auto process = mQueue.front();
+           mQueue.pop();
+           lock.unlock();
+
+           process();
+      }
+
+      });
+    }
+
+  }
+
+  std::future<void> fut;
+  std::queue<T> mQueue;
+  std::mutex mMutex;
+  bool runFlag;
+  std::condition_variable mCondVar;
+};
+
 #endif // PROCESSQUEUE_H
